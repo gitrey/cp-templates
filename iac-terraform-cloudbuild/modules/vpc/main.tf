d5f330e866aa691e36d9f2100955da942141070e
Andrey Shakirov <andreyshakirov@google.com>
2022-09-22 14:31:27 -0400

    updated subnet name

--- iac-terraform-cloudbuild/modules/vpc/main.tf
+++ iac-terraform-cloudbuild/modules/vpc/main.tf
@@ -22,7 +22,7 @@ module "vpc" {
 
   subnets = [
     {
-      subnet_name   = "${var.env}-subnet-01"
+      subnet_name   = "_APP_NAME-${var.env}-subnet-01"
       subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
       subnet_region = "_REGION"
     },
