e313347a29e7cb77d8c3171ec7eec38d9059d4e7
Andrey Shakirov <andreyshakirov@google.com>
2022-09-22 14:10:35 -0400

    Added subs values

--- iac-terraform-cloudbuild/modules/vpc/main.tf
+++ iac-terraform-cloudbuild/modules/vpc/main.tf
@@ -18,13 +18,13 @@ module "vpc" {
   version = "3.3.0"
 
   project_id   = "${var.project}"
-  network_name = "${var.env}"
+  network_name = "_APP_NAME-${var.env}"
 
   subnets = [
     {
       subnet_name   = "${var.env}-subnet-01"
       subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
-      subnet_region = "us-west1"
+      subnet_region = "_REGION"
     },
   ]
 
